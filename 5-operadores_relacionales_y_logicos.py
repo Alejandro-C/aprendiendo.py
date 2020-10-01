fcce6bf1b9f54027ba99ad577a1348e56f3d358c
Ever Carvajal <62713105+Alejandro-C@users.noreply.github.com>
2020-10-01 18:27:51 -0500

    operadores relacionales y logicos

---
+++ 5-operadores_relacionales_y_logicos.py
@@ -0,0 +1,22 @@
+''' OPERADORES RELACIONALES '''
+# ==    Igual que        
+# !=    Distinto que     
+# <     Menor que        
+# >     Mayor que        
+# <=    Menor o igual que
+# >=    Mayor o igual que
+
+print(5 == 7)  # Fase
+print('rojo' != 'verde')  # True
+print(8 < 12)  # True
+print(12 > 7)  # True
+print(12 <= 12)  # True
+print(4 >= 5)  # False
+
+
+''' OPERADORES LOGICOS '''
+# and   y
+# or    o 
+
+print(5 == 7 and 7 < 12)  # False
+print(12 == 12 or 15 < 7)  # True
