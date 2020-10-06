ae5b90dd593d98c96a2bb4447bd326570ff7a49c
Ever Carvajal <62713105+Alejandro-C@users.noreply.github.com>
2020-10-06 17:13:28 -0500

    importar modulos son usar namespace

--- 8-modulos_paquetes/app.py
+++ 8-modulos_paquetes/app.py
@@ -1,9 +1,20 @@
 import modulo  # importar un módulo que no pertenece a un paquete
 import paquete.modulo1  # importar un módulo que está dentro de un paquete
-import paquete.modulo2 as modulo2
-import paquete.subpaquete.submodulo1 as submodulo  # importar un módulo que está dentro de un subpaquete
+import paquete.modulo2
+import paquete.subpaquete.submodulo1  # importar un módulo que está dentro de un subpaquete
 
 print(modulo.mensaje)
 print(paquete.modulo1.mensaje)
-print(modulo2.mensaje)
-print(submodulo.mensaje)
+print(paquete.modulo2.mensaje)
+print(paquete.subpaquete.submodulo1.mensaje)
+
+# IMPORTAR MODULOS SIN USAR NAMESPACE
+# la palabra reservada 'as' es para darle un alias al modulo al que se esta importando
+from modulo import mensaje as mensaje1
+from paquete.modulo1 import mensaje as mensaje2
+from paquete.subpaquete.submodulo1 import mensaje as mensaje3
+
+print('-------------------------')
+print(mensaje1)
+print(mensaje2)
+print(mensaje3)
